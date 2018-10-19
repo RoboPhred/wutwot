59f2fe2f35952dc7f3a4d8ea3b47a2f859b073c9
RoboPhred <robophred@gmail.com>
2018-10-19 11:10:18 -0700

    Theming of theme grid

--- packages/homectrl-frontend/src/components/ThingGrid.tsx
+++ packages/homectrl-frontend/src/components/ThingGrid.tsx
@@ -1,42 +1,30 @@
 import * as React from "react";
 
-import GridList from "@material-ui/core/GridList";
-import GridListTile from "@material-ui/core/GridListTile";
-import { createStyles, withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 import ThingsDataSource from "@/services/homectrl/components/ThingsDataSource";
 import ThingGridItem from "./ThingGridItem";
 
-const cellSize = 240;
-const itemSize = 210;
-
 export interface ThingGridProps {
   className?: string;
 }
-const styles = createStyles({
-  gridItem: {
-    width: itemSize,
-    height: itemSize,
-    margin: 2
-  }
-});
-type Props = ThingGridProps & StyleProps<typeof styles>;
-const ThingGrid: React.SFC<Props> = ({ className, classes }) => (
+type Props = ThingGridProps;
+const ThingGrid: React.SFC<Props> = ({ className }) => (
   <ThingsDataSource>
     {({ things, error }) => (
       <div className={className}>
         {error && <span>{error.message}</span>}
         {things && (
-          <GridList cellHeight={cellSize} spacing={10}>
+          <Grid container spacing={8}>
             {things.map((thing, i) => (
-              <GridListTile key={i}>
-                <ThingGridItem className={classes.gridItem} thing={thing} />
-              </GridListTile>
+              <Grid item key={i} xs={6}>
+                <ThingGridItem thing={thing} />
+              </Grid>
             ))}
-          </GridList>
+          </Grid>
         )}
       </div>
     )}
   </ThingsDataSource>
 );
-export default withStyles(styles)(ThingGrid);
+export default ThingGrid;
