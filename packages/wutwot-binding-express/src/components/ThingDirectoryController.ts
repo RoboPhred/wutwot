2f57bf5d51585d9fa93901750bbb061d2b0512c6
RoboPhred <robophred@gmail.com>
2020-05-02 18:12:22 -0700

    Forms and property get/post

--- packages/wutwot-binding-express/src/components/ThingDirectoryController.ts
+++ packages/wutwot-binding-express/src/components/ThingDirectoryController.ts
@@ -1,9 +1,18 @@
 import { provides, singleton, inject, injectable } from "microinject";
-import { WutWot } from "@wutwot/core";
+import { WutWot, Thing, SchemaValidationError } from "@wutwot/core";
 import { ExpressController } from "@wutwot/servient-express";
-import { W3cWotContexts } from "@wutwot/td";
-import { controller, get } from "simply-express-controllers";
+import { Thing as TDThing, W3cWotTDContext } from "@wutwot/td";
+import HttpStatusCodes from "http-status-codes";
+import {
+  controller,
+  get,
+  pathParam,
+  body,
+  post,
+} from "simply-express-controllers";
 import { compact } from "jsonld";
+import createError from "http-errors";
+import keys from "lodash/keys";
 
 @injectable()
 @singleton()
@@ -18,10 +27,115 @@ export class ThingDirectoryController {
   })
   async getThings() {
     const things = Array.from(this._wutwot.things.values());
-    const lds = things.map((thing) => thing.toJSONLD());
-    const compressed = await Promise.all(
-      lds.map((ld) => compact(ld, W3cWotContexts.ThingDefinition)),
-    );
-    return compressed;
+    const tdPromises = things.map((thing) => this._getThingDefinition(thing));
+    const tds = await Promise.all(tdPromises);
+    return tds;
+  }
+
+  @get("/:thingId", {
+    description: "Gets a thing by its id",
+    tags: ["Thing"],
+  })
+  async getThing(@pathParam("thingId") thingId: string) {
+    const thing = this._wutwot.things.get(thingId);
+    if (!thing) {
+      throw createError(HttpStatusCodes.NOT_FOUND, "Thing not found.");
+    }
+    const tdThing = await this._getThingDefinition(thing);
+    return tdThing;
+  }
+
+  @get("/:thingId/properties/:propertyId", {
+    description: "Gets the value of a thing property",
+    tags: ["Thing", "Property"],
+  })
+  async getThingProperty(
+    @pathParam("thingId") thingId: string,
+    @pathParam("propertyId") propertyId: string,
+  ) {
+    const thing = this._wutwot.things.get(thingId);
+    if (!thing) {
+      throw createError(HttpStatusCodes.NOT_FOUND, "Thing not found.");
+    }
+    const prop = thing.properties.get(propertyId);
+    if (!prop) {
+      throw createError(HttpStatusCodes.NOT_FOUND, "Property not found.");
+    }
+    // Some properties can be undefined if their value is not known.
+    return prop.value ?? null;
+  }
+
+  @post("/:thingId/properties/:propertyId", {
+    description: "Sets the value of a thing property",
+    tags: ["Thing", "Property"],
+  })
+  async setThingProperty(
+    @pathParam("thingId") thingId: string,
+    @pathParam("propertyId") propertyId: string,
+    @body() value: any,
+  ) {
+    const thing = this._wutwot.things.get(thingId);
+    if (!thing) {
+      throw createError(HttpStatusCodes.NOT_FOUND, "Thing not found.");
+    }
+    const prop = thing.properties.get(propertyId);
+    if (!prop) {
+      throw createError(HttpStatusCodes.NOT_FOUND, "Property not found.");
+    }
+
+    try {
+      prop.setValue(value);
+    } catch (e) {
+      if (e instanceof SchemaValidationError) {
+        throw createError(
+          HttpStatusCodes.BAD_REQUEST,
+          "Body did not match the property schema.",
+        );
+      }
+      throw e;
+    }
+
+    // Doesn't appear to be any contract for the response.
+    return {};
+  }
+
+  private async _getThingDefinition(thing: Thing): Promise<TDThing> {
+    const ld = thing.toJSONLD();
+    let tdThing: TDThing = (await compact(ld, W3cWotTDContext)) as any;
+
+    // TODO: set base so form hrefs can be relative urls
+    //  Need to get what our root address is, which should be something
+    //  provided by servient-express
+
+    // TODO: wutwot core should handle forms
+    tdThing = this._injectForms(tdThing);
+    return tdThing;
+  }
+
+  private _injectForms(thing: TDThing): TDThing {
+    if (thing.properties) {
+      const propertyKeys = keys(thing.properties);
+      for (const propertyKey of propertyKeys) {
+        const property = thing.properties[propertyKey];
+        if (!property.forms) {
+          property.forms = [];
+        }
+
+        if (!property.writeOnly) {
+          property.forms.push({
+            op: "readproperty",
+            href: `/${thing.id}/properties/${propertyKey}`,
+          });
+
+          if (!property.readOnly) {
+            property.forms.push({
+              op: "writeproperty",
+              href: `/${thing.id}/properties/${propertyKey}`,
+            });
+          }
+        }
+      }
+    }
+    return thing;
   }
 }
