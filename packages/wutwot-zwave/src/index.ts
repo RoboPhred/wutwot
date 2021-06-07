fa3947942082c899a57c19905100d8bcd8de6dfb
RoboPhred <robophred@gmail.com>
2021-06-06 20:27:22 -0700

    Allow specifying plugins to load (missed commit files)

--- packages/wutwot-zwave/src/index.ts
+++ packages/wutwot-zwave/src/index.ts
@@ -8,3 +8,6 @@ export * from "./contracts";
 export * from "./types";
 export * from "./metadata-keys";
 export * from "./ZWavePlugin";
+
+import { ZWavePlugin } from "./ZWavePlugin";
+export default ZWavePlugin;
