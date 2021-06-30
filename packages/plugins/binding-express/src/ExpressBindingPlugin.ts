bd39fe7e4014f11bd7aaf567b17db926c4d5d244
RoboPhred <robophred@gmail.com>
2021-06-30 15:41:15 -0700

    Use fixed plugin id for express bindings as configuring more than one instance is not useful.

--- packages/plugins/binding-express/src/ExpressBindingPlugin.ts
+++ packages/plugins/binding-express/src/ExpressBindingPlugin.ts
@@ -4,21 +4,9 @@ import { WutWotPlugin } from "@wutwot/core";
 import { ThingDirectoryController } from "./components/ThingDirectoryController";
 import { HttpBindingFormProvider } from "./components/HttpBindingFormProvider";
 
-export interface ExpressBindingPluginOptions {
-  pluginId?: string;
-}
-
 export class ExpressBindingPlugin implements WutWotPlugin {
-  private _id: string = "bindings-express";
-
-  constructor(opts?: ExpressBindingPluginOptions) {
-    if (opts && opts.pluginId) {
-      this._id = opts.pluginId;
-    }
-  }
-
   get id(): string {
-    return this._id;
+    return "bindings-express";
   }
 
   onRegisterPublicServices(bind: BindFunction) {
